d10cc31a72b01c6a392d37e2b6da259469279edb
Mineaworld <mineawebdev@gmail.com>
2025-01-06 10:23:32 +0700

    Update Hero component video transition duration for improved responsiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -99,7 +99,7 @@ const Hero = () => {
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
             <div
               onClick={HandleMiniVideoClick}
-              className="origin-center scale-50 opacity-0 transition-all duration-400 ease-in hover:scale-100 hover:opacity-100 hover:"
+              className="origin-center scale-50 opacity-0 transition-all duration-200 ease-in hover:scale-100 hover:opacity-100"
             >
               <video
                 ref={nextVideoRef}
